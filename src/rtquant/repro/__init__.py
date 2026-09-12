b0873d101475a706758a8c0f0a11d3588a44990b
tangyunkai-hub <tangyunkai@gmail.com>
2026-09-12 22:37:25 +0800

    Export reproducibility manifest verifier

--- src/rtquant/repro/__init__.py
+++ src/rtquant/repro/__init__.py
@@ -1,1 +1,3 @@
-from .manifest import build_manifest
+from .manifest import ManifestIntegrityError, build_manifest, verify_manifest
+
+__all__ = ["ManifestIntegrityError", "build_manifest", "verify_manifest"]
