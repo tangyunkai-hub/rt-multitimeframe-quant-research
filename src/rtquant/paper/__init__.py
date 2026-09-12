8599d40b85ebd509b6db9c69fe5236bd6b6e9128
tangyunkai-hub <tangyunkai@gmail.com>
2026-09-12 22:35:54 +0800

    Export paper journal recovery API

--- src/rtquant/paper/__init__.py
+++ src/rtquant/paper/__init__.py
@@ -1,1 +1,10 @@
 from .runner import PaperState, process_bar
+from .journal import JournalIntegrityError, process_and_append, replay_journal
+
+__all__ = [
+    "PaperState",
+    "process_bar",
+    "JournalIntegrityError",
+    "process_and_append",
+    "replay_journal",
+]
