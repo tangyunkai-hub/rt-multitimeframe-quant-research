8bb7fb804cb2448da7b39dfa6565b1b3873fe284
tangyunkai-hub <tangyunkai@gmail.com>
2026-09-12 23:02:26 +0800

    Export prospective evidence embargo gate

--- src/rtquant/validation/__init__.py
+++ src/rtquant/validation/__init__.py
@@ -1,1 +1,15 @@
 from .paired import evaluate_paired_divergence
+from .prospective import (
+    CANDIDATE_B_FREEZE_UTC,
+    ProspectiveFloorStatus,
+    assess_candidate_b_information_floor,
+    evaluate_candidate_b_prospectively,
+)
+
+__all__ = [
+    "evaluate_paired_divergence",
+    "CANDIDATE_B_FREEZE_UTC",
+    "ProspectiveFloorStatus",
+    "assess_candidate_b_information_floor",
+    "evaluate_candidate_b_prospectively",
+]
