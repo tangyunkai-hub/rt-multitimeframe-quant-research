94ec71f6fe727cfeb71647b0b3425ede8928e4fa
tangyunkai-hub <tangyunkai@gmail.com>
2026-09-12 23:01:05 +0800

    Export operational paper session APIs

--- src/rtquant/paper/__init__.py
+++ src/rtquant/paper/__init__.py
@@ -1,10 +1,23 @@
 from .runner import PaperState, process_bar
-from .journal import JournalIntegrityError, process_and_append, replay_journal
+from .journal import (
+    JournalCursor,
+    JournalIntegrityError,
+    append_from_cursor,
+    process_and_append,
+    recover_journal,
+    replay_journal,
+)
+from .session import PaperSession, SessionLockError
 
 __all__ = [
     "PaperState",
     "process_bar",
+    "JournalCursor",
     "JournalIntegrityError",
+    "append_from_cursor",
     "process_and_append",
+    "recover_journal",
     "replay_journal",
+    "PaperSession",
+    "SessionLockError",
 ]
